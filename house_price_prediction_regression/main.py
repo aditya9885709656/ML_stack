0d0303c256f4d448062d6e753a4ab728325945d1
APAC\BDEV1KOR <nagavenkatabhagavatisaiaditya.devulapalli@in.bosch.com>
2023-06-13 18:30:29 +0530

    updated house price prediction use-case

---
+++ house_price_prediction_regression/main.py
@@ -0,0 +1,12 @@
+from data_prep import pre_processing
+import pandas as pd
+
+def main():
+    print("Reding Input data\n")
+    input_data = pd.read_csv("house_price.csv")
+    print("Data pre-process starts\n")
+    preprocess_data = pre_processing(input_data, "SalePrice")
+    
+    
+if __name__ == "__main__":
+    main()
